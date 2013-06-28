176889290dab60870a2d30916efe038acd45e4c4
Amos L King <amos.l.king@gmail.com>
2013-06-28 12:29:14 -0500

    Add aliases for tmux new and tmux attach
    Amos King @adkron <amos.l.king@gmail.com>

--- aliases.bash
+++ aliases.bash
@@ -3,3 +3,5 @@ alias ll='ls -la'
 alias la='ls -a'
 alias grep='GREP_COLOR="1;37;41" LANG=C grep --color=auto'
 alias pgrep='ps aux | grep'
+alias tn="tmux -2 new-session -s"
+alias tt="tmux -2 attach -t"
